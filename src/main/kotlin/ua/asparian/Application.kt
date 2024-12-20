185bb654562794bf85b177a0a6040e204e75986b
Dmytro Asparian <dima.supckik2004@gmail.com>
2024-12-20 19:36:46 +0200

    Updates for heroku

--- src/main/kotlin/ua/asparian/Application.kt
+++ src/main/kotlin/ua/asparian/Application.kt
@@ -1,9 +1,13 @@
 package ua.asparian
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    embeddedServer(Netty, port = System.getenv("PORT")?.toInt() ?: 8080) {
+        module()
+    }.start(wait = true)
 }
 
 fun Application.module() {
